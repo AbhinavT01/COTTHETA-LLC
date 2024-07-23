c4ca888c8c9e43e976cf9f3e6b2864cf8dd910a2
AbhinavT01 <156327966+AbhinavT01@users.noreply.github.com>
2024-07-23 07:59:19 +0530

    alternate file for doctext detection (structured )

---
+++ doc_text_detect2.py
@@ -0,0 +1,41 @@
+import os
+import re
+from google.cloud import vision
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'myservicegapi.json'
+
+def detect_document_text(image_path):
+    """Detects document text in an image."""
+    # Initialize the client
+    client = vision.ImageAnnotatorClient()
+
+    # Read the image file
+    with open(image_path, 'rb') as image_file:
+        content = image_file.read()
+
+    # Construct the image
+    image = vision.Image(content=content)
+
+    # Perform document text detection
+    response = client.document_text_detection(image=image)
+    annotations = response.full_text_annotation
+
+    if annotations:
+        # Remove extra spaces and ensure no leading spaces in the first line
+        formatted_text = re.sub(r'\s+', ' ', annotations.text).strip()
+
+        print('Detected document text:')
+        print(formatted_text)
+        return formatted_text
+    else:
+        return 'No document text detected.'
+
+    if response.error.message:
+        raise Exception(f'{response.error.message}')
+
+# if __name__ == "__main__":
+
+#     # Path to the image file
+#     image_path = './images1.jpg'
+    
+#     # Detect document text
+#     detect_document_text(image_path)
